fc638831c6ded92c447a03453310c6a1c2f8ef39
sfiteni <sfiteni@gmail.com>
2020-09-07 09:30:33 +0200

    Limiting the ship's range

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -13,7 +13,7 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+            (self.settings.screen_width, self.settings.screen_height), pygame.FULLSCREEN)
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
@@ -32,9 +32,13 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
                     
     
     def _update_screen(self):
